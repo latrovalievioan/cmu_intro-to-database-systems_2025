2db599a35079b59370ac90634cd8cc94def1b3c4
Ioan Latrovaliev <latrovalievioan@gmail.com>
2026-06-22 18:45:47 +0300

    ss

--- homework#1/q3_Judo_athlete_medals.sqlite.sql
+++ homework#1/q3_Judo_athlete_medals.sqlite.sql
@@ -1,15 +1,16 @@
+explain query plan
 select name, sum(count) as sum
 from (
-    select count(*) as count, athletes.name
+    select count(medals.winner_code) as count, athletes.name
     from athletes
-    join medals on athletes.code = medals.winner_code
+    left join medals on athletes.code = medals.winner_code
     where athletes.disciplines like "%judo%"
     group by athletes.name
     union all
-    select count(*) as count, athletes.name 
+    select count(medals.winner_code) as count, athletes.name 
     from teams
     join athletes on teams.athletes_code = athletes.code
-    join medals on teams.code = medals.winner_code
+    left join medals on teams.code = medals.winner_code
     where athletes.disciplines like "%judo%"
     group by athletes.name 
 )
