cc1b61cea003680c67e31fca526b02e076422b5f
Ioan Latrovaliev <latrovalievioan@gmail.com>
2026-06-27 13:16:56 +0300

    solve q4

--- homework#1/2024/q4_Athletics_venue_athletes.sqlite.sql
+++ homework#1/2024/q4_Athletics_venue_athletes.sqlite.sql
@@ -1,21 +1,3 @@
--- explain query plan
--- with 
---     all_athletes as (
---         select name, athletes.code as a_c, teams.code as t_c, athletes.country_code, athletes.nationality_code
---         from athletes
---         left join teams on teams.athletes_code = athletes.code
---     ),
---     results_from_venues_hosting_athletics as (
---         select results.participant_code as p_c
---         from results
---         join venues on results.venue = venues.venue
---         where venues.disciplines like "%Athletics%"
---     )
--- select * 
--- from results_from_venues_hosting_athletics as r
--- join all_athletes as a on r.p_c = a.a_c or r.p_c = a.t_c
--- group by name;
-
 with 
     all_athletes as (
         select name, athletes.code, athletes.country_code, athletes.nationality_code
@@ -31,13 +13,11 @@ with
         join venues on results.venue = venues.venue
         where venues.disciplines like '%''Athletics''%'
     )
-select * 
+select distinct name, ca.code, cb.code
 from results_from_venues_hosting_athletics as r
 join all_athletes as a on a.code = r.participant_code
-join countries on 
-group by name
-order by ;
-
-select count(*)
-from athletes a
-where a.country_code <> a.nationality_code;
+join countries ca on ca.code = a.country_code
+join countries cb on cb.code = a.nationality_code
+where ca.Latitude not NULL and ca.Longitude not NULL
+order by pow((ca.Latitude - cb.Latitude), 2) + pow((ca.Longitude - cb.Longitude), 2) desc,
+    name asc;
